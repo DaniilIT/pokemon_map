5a561872aa7415b77353c8bb2e08ee6e83bfa40b
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-04-14 20:43:05 +0300

    show russified text in admin UI

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,29 +3,37 @@ from django.utils import timezone
 
 
 class Pokemon(models.Model):
-	title = models.CharField(max_length=200)
-	image = models.ImageField(upload_to='pokemons', null=True, blank=True)
-	description = models.TextField(blank=True)
-	title_en = models.CharField(max_length=200, blank=True)
-	title_jp = models.CharField(max_length=200, blank=True)
+	title = models.CharField('Название', max_length=200)
+	title_en = models.CharField('Название на английском', max_length=200, blank=True)
+	title_jp = models.CharField('Название на японском', max_length=200, blank=True)
+	description = models.TextField('Описание', blank=True)
+	image = models.ImageField('Картинка', upload_to='pokemons', null=True, blank=True)
 	previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL,
-		related_name='next_evolution', null=True, blank=True)
+		related_name='next_evolution', verbose_name='Произошел от', null=True, blank=True)
+
+	class Meta:
+		verbose_name = 'Вид покемона'
+		verbose_name_plural = 'Виды покемонов'
 
 	def __str__(self):
 		return self.title
 
 
 class PokemonEntity(models.Model):
-	pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-	lat = models.FloatField()
-	lon = models.FloatField()
-	appeared_at = models.DateTimeField(default=timezone.now)
-	disappeared_at = models.DateTimeField(default=timezone.now)
-	level = models.IntegerField(default=1)
-	health = models.IntegerField(null=True, blank=True)
-	strength = models.IntegerField(null=True, blank=True)
-	defence = models.IntegerField(null=True, blank=True)
-	stamina = models.IntegerField(null=True, blank=True)
+	pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Вид покемона')
+	lat = models.FloatField('Широта')
+	lon = models.FloatField('Долгота')
+	appeared_at = models.DateTimeField('Появится в', default=timezone.now)
+	disappeared_at = models.DateTimeField('Исчезнет в', default=timezone.now)
+	level = models.IntegerField('Уровень', default=1)
+	health = models.IntegerField('Здоровье', null=True, blank=True)
+	strength = models.IntegerField('Атака', null=True, blank=True)
+	defence = models.IntegerField('Защита', null=True, blank=True)
+	stamina = models.IntegerField('Выносливость', null=True, blank=True)
+
+	class Meta:
+		verbose_name = 'Покемон'
+		verbose_name_plural = 'Покемоны'
 
 	def __str__(self):
 		return f'{self.pokemon.title}, level: {self.level}'
